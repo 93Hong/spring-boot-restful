b5b07c6bd9994d3635fbbe6a7db81deeb6928ffe
93Hong <sk2rldnr@gmail.com>
2017-07-09 23:17:15 +0900

    tutorial done

--- src/main/java/com/example/demo/HotelBooking.java
+++ src/main/java/com/example/demo/HotelBooking.java
@@ -13,7 +13,7 @@ import javax.persistence.Id;
 public class HotelBooking {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     private String hotelName;
